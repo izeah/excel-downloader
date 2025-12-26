bd16faebffb3bbcc952cd5d1a69198b241dabdd9
Faiz Mohammad Hafidza <faiz.hafidza@code.id>
2025-12-26 23:38:20 +0700

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./*.html"], // Optional: ensures Tailwind scans your HTML file
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
